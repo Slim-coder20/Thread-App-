209ae1ffafaa3e2924f6a8e545437942d8c0ce6b
slim <slimabida21@gmail.com>
2025-09-11 15:22:27 +0200

    création de l'accueil avec simulation de posts avc l'objet posts test OK

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,52 @@
 import ConnectedLayout from "@/components/ConnectedLayout/ConnectedLayout";
-
+import Post from "@/components/Post/Post";
 export default function Index() {
+  // On créé des posts pour les afficher dans la page d'accueil
+
+  const posts = [
+    {
+      _id: "1",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "jhondoe",
+      profile: "/picture.png",
+    },
+    {
+      _id: "2",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "Stevejob",
+      profile: "/picture.png",
+    },
+    {
+      _id: "3",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "Stevejob",
+      profile: "/picture.png",
+    },
+    {
+      _id: "4",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "Stevejob",
+      profile: "/picture.png",
+    },
+    {
+      _id: "5",
+      content: "Bienvenue sur mon tout nouveau profil Threads !",
+      pseudo: "Stevejob",
+      profile: "/picture.png",
+    },
+  ];
   return (
-    <ConnectedLayout>Accueil</ConnectedLayout>
-  )
+    <ConnectedLayout>
+      <div className="md:w-[700px] w-full mx-auto mt-10">
+        {/* New post  */}
+
+        {/* Posts */}
+        {posts.map((post) => (
+          <div key={post._id}>
+            <Post post={post} />
+          </div>
+        ))}
+      </div>
+    </ConnectedLayout>
+  );
 }
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+export default function Post({ post }) {
+  return (
+    <div className="mt-9 border border-b-gray-900 flex items-center gap-2">
+      {/* Photo */}
+      <div>
+        <Image
+          src={post.profile}
+          alt="User"
+          width={50}
+          height={50}
+          className="rounded-full object-cover mb-5"
+        />
+      </div>
+
+      {/* content  */}
+      <div className="  text-white w-full ">
+        {/* Infos  */}
+        <div className=" flex items-center justify-between ">
+          <Link href={`/@{post.pseudo}`}>
+            <b>{post.pseudo}</b>
+          </Link>
+          <div className=" text-sm text-gray-500">Il y a une heure</div>
+        </div>
+        {/* text */}
+        <div className=" mt-3 whitespace-pre-line ">{post.content}</div>
+      </div>
+    </div>
+  );
+}
