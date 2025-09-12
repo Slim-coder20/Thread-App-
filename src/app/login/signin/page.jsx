fbb57504d045625cb77a53c787276397fb5ddef4
slim <slimabida21@gmail.com>
2025-09-12 09:04:25 +0200

    Mise en  place de la page de connexion de l'application

---
+++ src/app/login/signin/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import Button from "@/components/Button/Button";
+
+export default function Signin() {
+  // function //
+  const prepareLogin = async (formData) => {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(email, password);
+    //await createUser( email, password);
+  };
+  return (
+    <div className="w-[440px] mx-auto">
+      <h1 className="text-center font-bold text-white text-xl flex  gap-2 items-center">
+        <Link href="/login">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 256 256"
+          >
+            <path
+              fill="currentColor"
+              d="M222 128a6 6 0 0 1-6 6H54.49l61.75 61.76a6 6 0 1 1-8.48 8.48l-72-72a6 6 0 0 1 0-8.48l72-72a6 6 0 0 1 8.48 8.48L54.49 122H216a6 6 0 0 1 6 6"
+            />
+          </svg>
+        </Link>
+        Connectez-vous
+      </h1>
+      {/* Form  */}
+      <form action={prepareLogin}>
+        <input
+          type="email"
+          name="email"
+          placeholder="adresse email"
+          className="bg-gray-800 block w-full mt-3 p-5 text-gray-300 rounded-xl "
+          required
+        />
+        <input
+          type="paswword"
+          name="password"
+          placeholder="mot de passe  "
+          className="bg-gray-800 block w-full mt-3 p-5 text-gray-300 rounded-xl "
+          required
+        />
+        <Button>Se connecter </Button>
+      </form>
+      <div className="flex justify-center items-center mt-4 ">
+        <div className=" border-t border-gray-500 w-1/4"></div>
+        <div className=" text-gray-500 mx-4 ">ou</div>
+        <div className=" border-t border-gray-500 w-1/4"></div>
+      </div>
+      <Link href="/login/signup">
+        <Button>Créer un compte</Button>
+      </Link>
+    </div>
+  );
+}
