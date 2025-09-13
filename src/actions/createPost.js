1a163c34bb824c26fe4ddb3f797705978f732afb
slim <slimabida21@gmail.com>
2025-09-13 19:50:56 +0200

    Mise en place de la suppresion du cache après création du post avec la méthode revalidatePath test OK

--- src/actions/createPost.js
+++ src/actions/createPost.js
@@ -2,6 +2,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { MongoClient } from "mongodb";
 import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
 
 export const createPost = async (formData) => {
   // Variable : On commence par vérfier que notre utilisateur est bien connecté //
@@ -12,40 +13,36 @@ export const createPost = async (formData) => {
     throw new Error("Vous devez être connecté.");
   }
 
-  // Récupération des champs de formulaire // 
-  const content = formData.get("content"); 
+  // Récupération des champs de formulaire //
+  const content = formData.get("content");
 
-
-  // validation simple // 
-  if(!content || content.trim() === ""){
+  // validation simple //
+  if (!content || content.trim() === "") {
     throw new Error("Le contenu du post est requis");
-    
   }
 
-  let client; 
+  let client;
   try {
-    // connexion à la base de donnée cluster // 
-    client = await MongoClient.connect(process.env.MONGODB_URI); 
-    // connexion a la base de donnée database // 
-    const db = client.db(); 
-    const posts = db.collection("posts"); 
+    // connexion à la base de donnée cluster //
+    client = await MongoClient.connect(process.env.MONGODB_URI);
+    // connexion a la base de donnée database //
+    const db = client.db();
+    const posts = db.collection("posts");
 
-    // Création du post // 
+    // Création du post //
     const post = {
-      content, 
-      userId: session.user.id, 
-      username: session.user.username, 
-      profile: session.user.profile, 
-      createAt: new Date(), 
-    }; 
-    await posts.insertOne(post); 
-    return { success: true }; 
-    
+      content,
+      userId: session.user.id,
+      username: session.user.username,
+      profile: session.user.profile,
+      createAt: new Date(),
+    };
+    await posts.insertOne(post);
+    return { success: true };
   } catch (error) {
     throw new Error("Erreur lors de la création du post :" + error.message);
-    
-  }finally {
-    if(client) await client.close(); 
+  } finally {
+    if (client) await client.close();
+    revalidatePath("/");
   }
-
 };
